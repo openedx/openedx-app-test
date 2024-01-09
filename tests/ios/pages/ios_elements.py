b5929426817a265a83a788a914c49e9cd9980f4f
Bilal Awan <bawan@2u.com>
2024-01-09 22:04:02 +0500

    Automate Login and Register screen
    - Create test logging directory
    
     - Create global methods to find element and scroll on screen

---
+++ tests/ios/pages/ios_elements.py
@@ -0,0 +1,13 @@
+"""
+   Module covers iOS screens' elements
+"""
+all_editfields = 'XCUIElementTypeTextField'
+all_buttons = 'XCUIElementTypeButton'
+all_textviews = 'XCUIElementTypeStaticText'
+all_pickwheels = 'XCUIElementTypePickerWheel'
+all_otherviews = 'XCUIElementTypeOther'
+all_textview_type = 'XCUIElementTypeTextView'
+
+# NEW LOGISTRATION SCREEN
+password_field = 'XCUIElementTypeSecureTextField'
+register_screen_heading = 'Register'
