098a4fbdf912f088003b05b41953c40727198b22
Bilal Awan <56023064+bilalawan321@users.noreply.github.com>
2024-03-29 23:23:33 +0500

    chore: automate whats new screen for ios (#13)
    - Create test and page files for whats new screen
    - Update values.py and ios_element files for whats new
    
    Fixes: LEARNER-9820

---
+++ tests/ios/pages/ios_whats_new.py
@@ -0,0 +1,85 @@
+"""
+    Whats New Page Module
+"""
+from appium.webdriver.common.mobileby import MobileBy
+from tests.ios.pages import ios_elements
+from tests.ios.pages.ios_base_page import IosBasePage
+
+
+class IosWhatsNew(IosBasePage):
+    """
+    Whats New screen
+    """
+
+    def get_close_button(self):
+        """
+        Get close button
+
+        Returns:
+            webdriver element: close Element
+        """
+
+        self.global_contents.wait_for_element_visibility(
+            self.driver,
+            ios_elements.whats_new_btn_next
+        )
+
+        return self.driver.find_element(MobileBy.NAME, 'close_button')
+
+    def get_whats_new_msg_title(self):
+        """
+        Get Whats New message title
+
+        Returns:
+            webdriver element: Whats New message title element
+        """
+
+        whats_new_msg_title = self.global_contents.wait_and_get_element(
+            self.driver,
+            ios_elements.whats_new_msg_title
+        )
+        return whats_new_msg_title
+
+    def get_whats_new_description(self):
+        """
+        Get Whats New description
+
+        Returns:
+            webdriver element: Whats New description element
+        """
+
+        whats_new_description = self.global_contents.wait_and_get_element(
+            self.driver,
+            ios_elements.whats_new_description
+        )
+        return whats_new_description
+
+    def get_next_btn(self):
+        """
+        Get Next button
+
+        Returns:
+            webdriver element: Next button element
+        """
+
+        next_btn = self.global_contents.wait_and_get_element(
+            self.driver,
+            ios_elements.whats_new_btn_next
+        )
+        return next_btn
+
+    def navigate_features(self):
+        """
+        Navigate between features
+
+        Returns:
+            webdriver element: Done Element
+        """
+
+        self.get_next_btn().click()
+
+        if self.get_next_btn().text == 'Done':
+            return self.get_next_btn()
+        else:
+            self.navigate_features()
+            return self.get_next_btn()
---
+++ tests/ios/tests/test_ios_whats_new.py
@@ -0,0 +1,77 @@
+"""
+    Whats New Test Module
+"""
+
+from tests.ios.pages.ios_landing import IosLanding
+from tests.ios.pages.ios_login import IosLogin
+from tests.ios.pages.ios_whats_new import IosWhatsNew
+from tests.common import values
+from tests.common.globals import Globals
+
+
+class TestIosWhatsNew:
+    """
+    Whats new screen's Test Case
+    """
+
+    def test_start_whats_new_smoke(self, set_capabilities, setup_logging):
+        """
+        Scenarios:
+            Verify Whats New screen is loaded successfully
+        """
+
+        setup_logging.info(f'Starting {TestIosWhatsNew.__name__} Test Case')
+        ios_landing = IosLanding(set_capabilities, setup_logging)
+        ios_login = IosLogin(set_capabilities, setup_logging)
+        global_contents = Globals(setup_logging)
+
+        if global_contents.whats_new_enable:
+            sign_in_button = ios_landing.get_sign_in_button()
+            assert ios_landing.get_sign_in_button().text == values.LOGIN
+            sign_in_button.click()
+            sign_in_title = ios_login.get_sign_in_title()
+            assert sign_in_title.text == values.LOGIN
+
+            email_field = ios_login.get_signin_username_textfield()
+            assert email_field.text == values.EMAIL_OR_USERNAME_IOS
+            email_field.send_keys(global_contents.login_user_name)
+
+            password_title = ios_login.get_signin_password_text()
+            assert password_title.text == values.PASSWORD
+            password_title.click()
+            password_field = ios_login.get_signin_password_textfield()
+            assert password_field.get_attribute('value') == values.PASSWORD
+            password_field.send_keys(global_contents.login_password)
+            password_title.click()
+            sign_in_button = ios_login.get_signin_button()
+            assert sign_in_button.text == values.LOGIN
+            sign_in_button.click()
+        else:
+            setup_logging.info('Whats New feature is not enabled')
+
+    def test_validate_ui_elements_smoke(self, set_capabilities, setup_logging):
+        """
+        Scenarios:
+            Verify following contents are visible on screen 
+                "Screen Title", "Cross Icon", "Main Feature Image",
+                    "Feature Title", "Feature Details", "Done"
+            Verify all screen contents have their default values
+        """
+
+        whats_new_page = IosWhatsNew(set_capabilities, setup_logging)
+        global_contents = Globals(setup_logging)
+
+        if global_contents.whats_new_enable:
+            close_btn = whats_new_page.get_close_button()
+            assert close_btn.text == values.WHATS_NEW_CLOSE_BUTTON
+
+            screen_title = global_contents.get_ios_all_static_text(set_capabilities)[0]
+            assert screen_title.text == values.WHATS_NEW_TITLE
+            assert whats_new_page.get_whats_new_msg_title().text
+            assert whats_new_page.get_whats_new_description().text
+            next_button = whats_new_page.get_next_btn()
+            assert next_button.text == values.WHATS_NEW_NEXT_BUTTON
+            assert whats_new_page.navigate_features().text == 'Done'
+            whats_new_page.get_next_btn().click()
+        else:
+            setup_logging.info('Whats New feature is not enabled')
