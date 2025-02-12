e8947af8c8e12d25a39e1b8cf2cb6c3fe07162b6
Bilal Awan <56023064+bilalawan321@users.noreply.github.com>
2025-02-12 23:01:39 +0500

    chore: automate course component navigation (#44)
    Fixes: LEARNER-10401

---
+++ tests/android/pages/android_course_home_tab.py
@@ -0,0 +1,69 @@
+"""
+    Course Home Page Module
+"""
+
+from tests.android.pages.android_base_page import AndroidBasePage
+from tests.common import values
+from selenium.common.exceptions import NoSuchElementException
+
+
+class AndroidCourseHomeTab(AndroidBasePage):
+    """
+    Course Home Tab screen
+    """
+
+    def component_navigation(self, retries=5):
+        """
+        Navigate between components
+
+        Args:
+            retries (int): Maximum number of retries to find the finish button.
+
+        Returns:
+            webdriver element: finish Element
+        """
+        if retries == 0:
+            raise Exception("Finish button not found after maximum retries")
+
+        self.get_next_btn().click()
+
+        if self.get_finish_button():
+            return self.get_finish_button()
+        else:
+            return self.component_navigation(retries=retries - 1)
+
+    def get_finish_button(self):
+        """
+        Get finish button
+
+        Returns:
+            element: finish element
+        """
+
+        try:
+            return self.global_contents.get_element_by_exact_text_android(
+                self.driver, values.COURSE_COMPONENT_FINISH_BUTTON)
+        except NoSuchElementException:
+            return None
+
+    def get_next_btn(self):
+        """
+        Get next button
+
+        Returns:
+            element: next button element
+        """
+
+        return self.global_contents.get_element_by_text(
+            self.driver, values.COURSE_COMPONENT_NEXT_BUTTON)
+
+    def get_prev_btn(self):
+        """
+        Get prev button
+
+        Returns:
+            element: prev button element
+        """
+
+        return self.global_contents.get_element_by_text(
+            self.driver, values.COURSE_COMPONENT_PREVIOUS_BUTTON)
